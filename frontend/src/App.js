fcbef9cb210064ac529aff4dd246403109794b7a
HugoCls <hugocls.pro@gmail.com>
2024-12-12 20:35:52 +0100

    Change backend url for env variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [imageUrl, setImageUrl] = useState("");
   const [exifData, setExifData] = useState({});
 
-  const backendUrl = "http://0.0.0.0/api"; // Backend base URL
+  const backendUrl = process.env.REACT_APP_BACKEND_URL; // Use the environment variable
 
   const axiosInstance = axios.create({
     baseURL: backendUrl,
